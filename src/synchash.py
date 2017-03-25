3877337e632c0d9714e7d1762cd6b95a420463e4
skywhat <i@skywhat.com>
2017-03-25 17:51:39 +0800

    synchash complete
    完成synchash编写，未测试

--- src/synchash.py
+++ src/synchash.py
@@ -6,8 +6,10 @@ import path
 
 
 class FileHashList(object):
-    # TODO 测试node != None，是否可以这样写 skywhat
-    # TODO 是否可以显式定义属性 skywhat
+    #  测试node != None，是否可以这样写 skywhat
+    #  写成 node is not None
+    #  是否可以显式定义属性 skywhat
+    #  初始化定义属性，分清类属性与实例属性的区别
 
     def __init__(self):
         self.hash_list = [None]*hash_algorithm.sync_hash_length
@@ -15,7 +17,7 @@ class FileHashList(object):
     # 根据文件名把FileHashNode对象存入到哈希数组链表中的适当位置
     def insert(self, node):
         offset = hash_algorithm.get_hash_offset(node.get_name_hashcode())
-        if self.hash_list[offset] == None:
+        if self.hash_list[offset] is None:
             self.hash_list[offset] = node
         else:
             node.set_next_node(self.hash_list[offset])
@@ -29,7 +31,7 @@ class FileHashList(object):
     def find_by_name_hashcode(self, name_hashcode):
         offset = hash_algorithm.get_hash_offset(name_hashcode)
         node = self.hash_list[offset]
-        while node != None:
+        while node is not None:
             if name_hashcode == node.get_name_hashcode():
                 return node
             else:
@@ -39,20 +41,31 @@ class FileHashList(object):
     # 根据文件名hash删除节点
     # 出现异常返回 -1
     # 找不到name_hashcode对应的节点返回 -2
-    # TODO 如果被删掉的node是FileHashList某条链表中剩下的最后一个节点了，则要把这个数组元素置为None，如果不理解这段话找shiweihua
+    #  如果被删掉的node是FileHashList某条链表中剩下的最后一个节点了，则要把这个数组元素置为None，如果不理解这段话找shiweihua
     def delete_by_name_hashcode(self, name_hashcode):
-        # TODO 根据相对路径删除节点  skywhat
-        pass
+        #  根据相对路径删除节点  skywhat
+        offset=hash_algorithm.get_hash_offset(name_hashcode)
+        node=self.hash_list[offset]
+        while node is not None and node.name_hashcode != name_hashcode:
+            node=node.next_node
+        if node is None:
+            return -1
+        if node.next_node is None:
+            node=None
+        else:
+            node.path=node.next_node.path
+            node.name_hashcode=node.next_node.name_hashcode
+            node.content_hashcode=node.next_node.content_hashcode
+            node.next_node=node.next_node.next_node
 
     # 根据name_hashcode修改content_hashcode
     def change_content_hashcode_by_name_hashcode(self, name_hashcode, content_hashcode):
-        # TODO 根据name_hashcode修改content_hashcode  skywhat
-        pass
-
-    # 根据name_hashcode修改flag
-    def change_flag_by_name_hashcode(self, name_hashcode):
-        # TODO 根据name_hashcode修改flag  skywhat
-        pass
+        # 根据name_hashcode修改content_hashcode  skywhat
+        offset=hash_algorithm.get_hash_offset(name_hashcode)
+        node=self.hash_list[offset]
+        while node is not None and node.name_hashcode!=name_hashcode:
+            node=node.next_node
+        node.content_hashcode=content_hashcode
 
 
 class FileHashNode(object):
@@ -88,11 +101,19 @@ class FileHashNode(object):
     def set_path(self, path):
         self.path = path
 
-    # TODO 判断该节点的content_hash是否和传入的节点相同 skywhat
+    # 判断该节点的content_hash是否和传入的节点相同 skywhat
+    # 若相同，返回True，不相同，返回False
     def is_equal_content_hash(self, node):
-        pass
+        if self.name_hashcode==node.name_hashcode and self.content_hashcode==node.content_hashcode:
+            return True
+        else:
+            return False
 
 
-# TODO 遍历本地目录，填充一个sync_hash数组链表 返回一个填满的sync_hash数组链表 skywhat
+#  遍历本地目录，填充一个sync_hash数组链表 返回一个填满的sync_hash数组链表 skywhat
 def fill_sync_hash_list(sync_hash_list):
-    pass
+    path_list=path.get_all_file_path(path.local_path)
+    for relative_path in path_list:
+        node=FileHashNode(path.local_path,relative_path)
+        sync_hash_list.insert(node)
+    return sync_hash_list
