db9f90b1438e56698268ff6f8cb5c03ced913283
liguoxiong <979760524@qq.com>
2017-04-21 14:37:48 +0800

    内存测试的代码

---
+++ profile/test/test2.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+# 内存测试的例子
+# 使用方法：在本目录下执行 "python -m memory_profiler test2.py"
+
+@profile
+def my_func():
+    a = [1] * (10 ** 6)
+    b = [2] * (2 * 10 ** 7)
+    del b
+    return a
+
+
+# 使用方法：在本目录下执行 "python -m memory_profiler test2.py"
+if __name__ == '__main__':
+    my_func()
